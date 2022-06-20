4e7890393dff8d5d64bac966a7612af7ca56403e
straktormedia <96350296+straktormedia@users.noreply.github.com>
2022-06-20 22:23:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,19 +6,15 @@ const animation = anime({
   autoplay: false,
 });
 
-// animation.play();
+const section2 = document.querySelector(".section-2");
 
-// Scroll on div function
-const scrollOnDiv = function (divSelector, speed = 1, offset = 0) {
-  return (
-    (window.pageYOffset -
-      document.querySelector(divSelector).offsetTop +
-      offset) /
-    (speed * 100)
-  );
+// Animate on scroll
+const animateOnScroll = function (div, speed = 100, offset = 0) {
+  const scrollPercent = window.pageYOffset - div.offsetTop;
+  return (scrollPercent + offset) / speed;
 };
 
-// Scroll Listener
+// Scroll listener
 window.onscroll = function () {
-  animation.seek(scrollOnDiv(".section-2") * animation.duration);
+  animation.seek(animateOnScroll(section2, 1000, 200) * animation.duration);
 };
